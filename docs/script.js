9c085fdebaeece8c3b67b2ab4b1eafeabb4999e6
marioviana07 <mario_viana_2005@yahoo.com>
2020-06-10 11:49:15 -0700

    changes

---
+++ docs/script.js
@@ -0,0 +1,219 @@
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+//arrays of characters 
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCases = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCases = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+var generateButton = document.getElementById('generate')
+generateButton.addEventListener('click', writePassword)
+
+// // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+// Prompt number entered is between 8 and 128
+function generatePassword() {
+  var passwordLength = Number(prompt("How many characters would you like your password to be?"));
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128)
+    passwordLength = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+  var upperCases = confirm("Would you like to use uppercase letters?");
+  var lowerCases = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
+
+  while (!upperCases && !lowerCases && !numbers && !specialCharacters) {
+    alert("You must select at least one character type!");
+    upperCases = confirm("Would you like to use uppercase letters?");
+    lowerCases = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confirm("Would you like to use special characters?");
+  }
+  // Minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
+
+
+  // Empty numbers, lowerCases, upperCases & specialCharacters return with something
+
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumspecialCharacters = "";
+
+
+  // Generator functions**
+  var functionArray = {
+    getNumbers: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+
+    getLowerCases: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+    },
+
+    getUpperCases: function () {
+      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    },
+
+    getsymbols: function () {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+    }
+
+  };
+
+  //selected ok for all and uses empty minimums from above
+
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
+
+  }
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+
+  }
+
+  if (symbols === true) {
+    minimumspecialCharacters = functionArray.getsymbols();
+    minimumCount++;
+
+  }
+
+  // empty string variable for the for loop below
+  var randomPasswordGenerated = "";
+
+  // loop getting random characters
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4);
+
+    randomPasswordGenerated += randomNumberPicked;
+
+  }
+
+  // Characters are added to the password
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumspecialCharacters;
+
+
+  return randomPasswordGenerated;
+}
+
+
+//Copy to clipboard button
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("password");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+
+
+}
+
